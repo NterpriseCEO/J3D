85bfeb7048764f6086e7af8190a3ca6a8314148c
NterpriseCEO <nuzumalex@gmail.com>
2025-03-10 01:22:57 +0000

    Added a XYZ coords indicator and a xyz=0 floor mesh

--- src/app/classes/EnvironmentSet.ts
+++ src/app/classes/EnvironmentSet.ts
@@ -1,6 +1,5 @@
-import { ArcRotateCamera, Engine, GizmoManager, PointLight, Scene, Vector3 } from "@babylonjs/core";
+import { ArcRotateCamera, Color3, Color4, CreateDisc, CreateGround, CreateLines, CreatePlane, CreateSphere, DynamicTexture, Engine, GizmoManager, Mesh, PointLight, Scene, StandardMaterial, Vector3, Viewport } from "@babylonjs/core";
 import { BehaviorSubject, Observable, of, Subject } from "rxjs";
-import { SetObject } from "./objects/shapes/Object";
 import { BaseObject } from "./objects/BaseObject";
 import { LightObject } from "./objects/lights/Lights";
 
@@ -29,6 +28,7 @@ export class EnvironmentSet {
 	initSet() {
 		this.engine = new Engine(this.canvas, true);
 		this.scene = new Scene(this.engine);
+		this.scene.clearColor = new Color4(0.2, 0.2, 0.2, 1);
 
 		this.camera = new ArcRotateCamera("Camera", -Math.PI / 2, Math.PI / 4, 15, Vector3.Zero());
 		this.camera.allowUpsideDown = false;
@@ -37,21 +37,52 @@ export class EnvironmentSet {
 		this.camera.setTarget(Vector3.Zero());
 		this.camera.attachControl(this.canvas, true);
 
+		// Camera 2 is for the XYZ indicator visual
+		const camera2 = new ArcRotateCamera('CAM2', -Math.PI / 4, 0.4 * Math.PI, 160, Vector3.Zero(), this.scene);
+		camera2.layerMask = 0x10000000;
+		camera2.viewport = new Viewport(0, 0, 0.1, 0.3);
+		// Camera 3 is for the XYZ text
+		const camera3 = new ArcRotateCamera('CAM3', -Math.PI / 4, 0.4 * Math.PI, 160, Vector3.Zero(), this.scene);
+		camera3.layerMask = 0x20000000;
+		camera3.viewport = new Viewport(0, 0, 0.1, 0.3);
+
+		this.scene.activeCameras?.push(camera2);
+		this.scene.activeCameras?.push(camera3);
+		this.scene.activeCameras?.push(this.camera);
+
+		// Need to make object names starting with _J3D illegal 
+		const originPoint = CreateGround("_J3DSceneOriginPoint", {
+			width: 10,
+			height: 10,
+			subdivisions: 10
+		}, this.scene);
+		const originPointMaterial = new StandardMaterial("OriginPointMaterial");
+		originPointMaterial.wireframe = true;
+		originPointMaterial.emissiveColor = new Color3(0.9, 0.9, 0.9);
+		originPoint.material = originPointMaterial;
+
 		// Shows the sizing/rotation/positioning gizmo on the screen when objects are clicked
 		this.gizmoManager = new GizmoManager(this.scene);
 		this.gizmoManager.positionGizmoEnabled = true;
 		this.gizmoManager.rotationGizmoEnabled = true;
 		this.gizmoManager.scaleGizmoEnabled = true;
+		// Prevents gizmo attachment on mouse down
+		this.gizmoManager.usePointerToAttachGizmos = false;
 
 		this.gizmoManager.onAttachedToMeshObservable.add(mesh => {
 			let isPointLight = false;
+
+			// Ignores the XYZ Coords Meshes
+			if(mesh?.id.startsWith("_J3D")) {
+				this.gizmoManager.attachToMesh(null);
+			}
+
 			this.selectedObjectIndex.next(this.setObjects.findIndex(object => {
 				// Determines which object component name to compare with
 				// name of the clicked mesh
 				const name = (object as LightObject)?.lightSphere
 					? (object as LightObject).lightSphere.name : object.name;
 				isPointLight = !!(object as LightObject)?.lightSphere;
-				console.log(isPointLight, typeof object);
 				return mesh?.name === name;
 			}));
 			this.gizmoManager.rotationGizmoEnabled = !isPointLight;
@@ -86,13 +117,22 @@ export class EnvironmentSet {
 			}
 		}
 
-		this.scene.onPointerUp = () => {
+		this.scene.onPointerUp = (e, pickResult) => {
 			// if no dragging and no object selected, deselects the current objects
-			if(!this.objectHitDetected && !this.isDragging) {
+			if (this.objectHitDetected && pickResult?.pickedMesh  && !this.isDragging) {
+				this.gizmoManager.attachToMesh(pickResult.pickedMesh);
+			}else if(!this.objectHitDetected && !this.isDragging) {
 				this.gizmoManager.attachToMesh(null);
-				this.selectedObjectIndex.next(-1);
 			}
 		}
+
+		this.showAxis();
+
+		this.scene.registerBeforeRender(() => {
+			// Orients cameras 2 and 3 to the scene camera
+			camera3.alpha = camera2.alpha = this.camera.alpha;
+			camera3.beta = camera2.beta = this.camera.beta;
+		});
 
 		this.engine.runRenderLoop(() => {
 			this.scene.render();
@@ -131,5 +171,63 @@ export class EnvironmentSet {
 	deselectObject() {
 		this.selectedObjectIndex.next(-1);
 		this.gizmoManager.attachToMesh(null);
+	}
+
+	private showAxis() {
+		const size = 50;
+
+		const xLine = CreateLines('_J3DCoordsIndicatorLineX', {
+			points: [
+				Vector3.Zero(), new Vector3(size, 0, 0), new Vector3(size * 0.95, 0.05 * size, 0),
+				new Vector3(size, 0, 0), new Vector3(size * 0.95, -0.05 * size, 0)
+			]
+		}, this.scene);
+		const yLine = CreateLines('_J3DCoordsIndicatorLineY', {
+			points: [
+				Vector3.Zero(), new Vector3(0, size, 0), new Vector3(-0.05 * size, size * 0.95, 0),
+				new Vector3(0, size, 0), new Vector3(0.05 * size, size * 0.95, 0)
+			]
+		}, this.scene);
+		const zLine = CreateLines('_J3DCoordsIndicatorLineZ', {
+			points: [
+				Vector3.Zero(), new Vector3(0, 0, size), new Vector3(0, -0.05 * size, size * 0.95),
+				new Vector3(0, 0, size), new Vector3(0, 0.05 * size, size * 0.95)
+			]
+		}, this.scene);
+		const indicatorSphere = CreateSphere('_J3DCoordsIndicatorBall', { segments: 24, diameter: size * .2 }, this.scene);
+		const indicatorMaterial = new StandardMaterial("_J3DCoordsIndicatorBallNaterial");
+		indicatorMaterial.emissiveColor = Color3.White();
+		indicatorSphere.material = indicatorMaterial;
+		indicatorSphere.isPickable = false;
+
+		const cx = this._textPlane('X');
+		const cy = this._textPlane('Y');
+		const cz = this._textPlane('Z');
+
+		[xLine, yLine, zLine, indicatorSphere].forEach(mesh => mesh.layerMask = 0x10000000);
+
+		xLine.color = new Color3(1, 0, 0);
+		cx.position = new Vector3(0.9 * size, -0.05 * size, 0);
+		yLine.color = new Color3(0, 1, 0);
+		cy.position = new Vector3(0, 0.9 * size, -0.05 * size);
+		zLine.color = new Color3(0, 0, 1);
+		cz.position = new Vector3(0, 0.05 * size, 0.9 * size);
+	}
+
+	private _textPlane(text: string): Mesh {
+		const dynamicTexture = new DynamicTexture('_J3DCoordsIndicatorTextTexture' + text, 128, this.scene, true);
+		const indicatorText = CreatePlane('_J3DCoordsIndicatorText' + text, { size: 60 }, this.scene);//might need true here
+		indicatorText.isPickable = false;
+		dynamicTexture.hasAlpha = true;
+		dynamicTexture.drawText(text, null, null, 'bold 20px Arial', "white", 'transparent', true);
+		const material = new StandardMaterial('matXYZ', this.scene);
+		material.emissiveColor = Color3.White();
+		material.diffuseTexture = dynamicTexture;
+		indicatorText.material = material;
+		indicatorText.material.backFaceCulling = false;
+		indicatorText.billboardMode = Mesh.BILLBOARDMODE_ALL;
+		indicatorText.layerMask = 0x20000000;
+
+		return indicatorText;
 	}
 }
